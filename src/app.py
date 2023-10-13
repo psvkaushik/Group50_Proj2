b09049e03c59efdea578c0f8bc8ddd482fe9a8f9
psvkaushik <psvkaushik@gmail.com>
2023-10-13 16:52:43 -0400

    added clone repo script

--- src/app.py
+++ src/app.py
@@ -1,5 +1,12 @@
 from flask import Flask, request, render_template
-import create_repo  # Import your Python script
+import create_repo # Import your Python script
+import delete_repo
+import yaml
+
+file_path = r'C:\Users\psvka\OneDrive\Desktop\fall23\CSC519\CSC-519-WS-5\vars.yaml'
+with open(file_path, 'r') as file:
+    data = yaml.safe_load(file)
+token = data['github_token']
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -11,7 +18,7 @@ def index():
 def create_github_repo():
     # Get the repo name and token from the form
     repo_name = request.form['repoName']
-    token = request.form['token']
+    #token = request.form['token']
 
     # Call your create_github_repo function from your Python script
     response = create_repo.create_github_repo(token, repo_name)
@@ -22,6 +29,7 @@ def create_github_repo():
         return "Repository created successfully!"
     else:
         return f"Error creating repository. Status code: {response.status_code}"
+    
 
 if __name__ == '__main__':
     app.run(debug= True, port=5020)
