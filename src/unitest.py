8910b69257ee5c778947a0ee08143ca81a1ed460
VaishnaviNaik96 <111774368+VaishnaviNaik96@users.noreply.github.com>
2023-10-14 18:20:44 -0400

    Update unitest.py
    unit test for gitpull.py

--- src/unitest.py
+++ src/unitest.py
@@ -37,6 +37,43 @@ class TestCreateGithubRepo(unittest.TestCase):
         # Assert
         self.assertEqual(response.status_code, 400)
         self.assertTrue(mock_post.called)
+        
+    @patch('requests.get')
+    def test_successful_request(self, mock_get):
+        # Mock the requests.get method to return a 200 response with content
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.text = 'Test content'
+
+        # Call the function with mock data
+        pull_file_from_github('your_token', 'owner', 'repo', 'file.txt', 'local.txt')
+
+        # Assertions
+        mock_get.assert_called_with(
+            'https://raw.githubusercontent.com/owner/repo/main/file.txt',
+            headers={
+                'Authorization': 'token your_token',
+                'Accept': 'application/vnd.github.v3.raw'
+            }
+        )
+
+    @patch('requests.get')
+    def test_failed_request(self, mock_get):
+        # Mock the requests.get method to return a non-200 response
+        mock_get.return_value.status_code = 404
+        mock_get.return_value.text = 'Not Found'
+
+        # Call the function with mock data
+        pull_file_from_github('your_token', 'owner', 'repo', 'file.txt', 'local.txt')
+
+        # Assertions
+        mock_get.assert_called_with(
+            'https://raw.githubusercontent.com/owner/repo/main/file.txt',
+            headers={
+                'Authorization': 'token your_token',
+                'Accept': 'application/vnd.github.v3.raw'
+            }
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
