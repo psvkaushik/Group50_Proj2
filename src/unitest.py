2a3ce8ac804457d127804ab81b7c25e8f07c52ad
VaishnaviNaik96 <111774368+VaishnaviNaik96@users.noreply.github.com>
2023-10-14 18:36:00 -0400

    Update unitest.py
    All test cases

--- src/unitest.py
+++ src/unitest.py
@@ -164,6 +164,86 @@ class TestCreateGithubRepo(unittest.TestCase):
             }
         )
 
+    @patch('requests.delete')
+    def test_delete_repo_success(self, mock_delete):
+        # Mock the requests.delete method to return a successful response
+        mock_delete.return_value.status_code = 204  # HTTP status code for successful deletion
+
+        # Call the function with mock data
+        response = delete_github_repo(github_token, 'username', 'repo-to-delete')
+
+        # Assertions
+        self.assertEqual(response.status_code, 204)
+
+        # Ensure that requests.delete was called with the expected arguments
+        mock_delete.assert_called_with(
+            f'https://api.github.com/repos/username/repo-to-delete',
+            headers={
+                'Authorization': 'token '+ github_token,
+                'Accept': 'application/vnd.github.v3+json'
+            }
+        )
+
+    @patch('requests.delete')
+    def test_delete_repo_failure(self, mock_delete):
+        # Mock the requests.delete method to return a failed response
+        mock_delete.return_value.status_code = 404  # Simulate a not found error
+
+        # Call the function with mock data
+        response = delete_github_repo(github_token, 'username', 'repo-to-delete')
+
+        # Assertions
+        self.assertEqual(response.status_code, 404)
+
+        # Ensure that requests.delete was called with the expected arguments
+        mock_delete.assert_called_with(
+            f'https://api.github.com/repos/username/repo-to-delete',
+            headers={
+                'Authorization': github_token,
+                'Accept': 'application/vnd.github.v3+json'
+            }
+        )
+
+    @patch('requests.post')
+    def test_fork_repo_success(self, mock_post):
+        # Mock the requests.post method to return a successful response
+        mock_post.return_value.status_code = 202  # HTTP status code for accepted (accepted for asynchronous processing)
+
+        # Call the function with mock data
+        response = fork_repo('target_user', 'target_repo', github_token)
+
+        # Assertions
+        self.assertEqual(response.status_code, 202)
+
+        # Ensure that requests.post was called with the expected arguments
+        mock_post.assert_called_with(
+            'https://api.github.com/repos/target_user/target_repo/forks',
+            headers={
+                'Authorization': 'token '+github_token,
+                'Accept': 'application/vnd.github.v3+json'
+            }
+        )
+
+    @patch('requests.post')
+    def test_fork_repo_failure(self, mock_post):
+        # Mock the requests.post method to return a failed response
+        mock_post.return_value.status_code = 404  # Simulate a not found error
+
+        # Call the function with mock data
+        response = fork_repo('target_user', 'target_repo', github_token)
+
+        # Assertions
+        self.assertEqual(response.status_code, 404)
+
+        # Ensure that requests.post was called with the expected arguments
+        mock_post.assert_called_with(
+            'https://api.github.com/repos/target_user/target_repo/forks',
+            headers={
+                'Authorization': 'token '+github_token,
+                'Accept': 'application/vnd.github.v3+json'
+            }
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
