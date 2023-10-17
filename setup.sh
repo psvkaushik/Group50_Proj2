3d94b05b711a307fb2a5ab5f612027730b44690b
VaishnaviNaik96 <111774368+VaishnaviNaik96@users.noreply.github.com>
2023-10-16 23:51:12 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#Ask for input from user
+
+read -p "Enter username please: " Username
+read -p "Enter your auth token please: " Token
+
+
+# Check if pip is installed
+
+if command -v pip &>/dev/null; then
+
+    PIP_COMMAND=pip
+
+else
+
+    echo "pip is not installed. Attempting to install it..."
+
+    sudo apt-get update
+
+    sudo apt-get install -y python3-pip  # Install pip
+
+    PIP_COMMAND=pip  # Use pip
+
+fi
+
+# Set GitHub environment variable
+
+export GITHUB_TOKEN=$Token
+
+# Install the required Python packages using the determined pip command
+
+$PIP_COMMAND install requests flask PyYAML
+
+# Check if the installation was successful
+
+if [ $? -eq 0 ]; then
+
+    echo "Packages installed successfully."
+
+else
+
+    echo "Package installation failed."
+
+fi
