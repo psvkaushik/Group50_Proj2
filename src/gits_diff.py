bfb662e25bcd135af9b5fbc8b0afc1ce2da15119
VaishnaviNaik96 <111774368+VaishnaviNaik96@users.noreply.github.com>
2023-10-17 14:35:20 -0400

    Update gits_diff.py
    response code and token updated

--- src/gits_diff.py
+++ src/gits_diff.py
@@ -1,11 +1,11 @@
 import requests
 
 
-def get_github_commit_diff(owner, repo, branch):
+def get_github_commit_diff(owner, repo, branch, github_token):
     try:
         # Replace 'YOUR_GITHUB_TOKEN' with your GitHub personal access token or use other authentication methods.
         headers = {
-            'Authorization': ' ',  ####TOKEN
+            'Authorization': f'token {github_token}',  ####TOKEN
         }
 
         # Define the API URL to fetch the latest commit on a specific branch.
@@ -23,19 +23,20 @@ def get_github_commit_diff(owner, repo, branch):
 
             # Send a GET request to the GitHub API to retrieve the difference.
             response = requests.get(diff_api_url, headers=headers)
-
-            if response.status_code == 200:
-                diff_data = response.json()
-                print("Difference since the last commit:")
-                print(diff_data['files'])
-            else:
-                print(f"Error: Unable to fetch the difference - Status Code {response.status_code}")
-                print(response.text)
-                return False
+            return response
+            #if response.status_code == 200:
+            #    diff_data = response.json()
+            #    print("Difference since the last commit:")
+            #    print(diff_data['files'])
+            #else:
+            #    print(f"Error: Unable to fetch the difference - Status Code {response.status_code}")
+            #    print(response.text)
+            #    return False
         else:
-            print(f"Error: Unable to fetch the latest commit - Status Code {response.status_code}")
-            print(response.text)
-            return False
+            #print(f"Error: Unable to fetch the latest commit - Status Code {response.status_code}")
+            #print(response.text)
+            #return False
+            return response
 
     except Exception as e:
         print("ERROR: gits diff command caught an exception")
@@ -49,5 +50,6 @@ if __name__ == "__main__":
     owner = ' '  # Replace with your GitHub username or organization name.
     repo = ' '  # Replace with the name of the GitHub repository.
     branch = ' '  # Specify the branch you want to compare with the last commit.
+    github_token = '  '  # Enter token 
 
     get_github_commit_diff(owner, repo, branch)
