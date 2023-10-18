65de9e6efc850589ea998088cb934f9eb8804973
Ayush Agarwal <agarwalayush1996@gmail.com>
2023-10-18 13:09:23 -0400

    Update gits_delete.py copyright

--- src/gits_delete.py
+++ src/gits_delete.py
@@ -1,3 +1,12 @@
+"""
+Copyright (C) 2023 G.I.T.S.3 - All Rights Reserved
+You may use, distribute, and modify this code under the
+terms of the MIT license.
+You should have received a copy of the MIT license with
+this file. If not, please write to: secheaper@gmail.com
+
+"""
+
 import requests
 
 def delete_github_repo(PAT, username, repo_name_to_delete):
