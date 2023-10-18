6ddf22797945ba05bb472016a781b22c5cc89cbb
psvkaushik <psvkaushik@gmail.com>
2023-10-18 15:11:03 -0400

    added gits_push

--- src/gits_push.py
+++ src/gits_push.py
@@ -8,26 +8,25 @@ this file. If not, please write to: secheaper@gmail.com
 """
 
 import subprocess
-from gits_checkbranch import check_branch_exists
+from gits_commit import commit
 
-def create_pr(PAT, user_name, repo_name, dir_path,  branch_name='main', file_path='.', commit_message='user forgot to add message'):
-    result = []
-    check_if_exists = check_branch_exists(PAT, user_name, repo_name, branch_name)
-    if check_if_exists.status_code == 200:
-        command = ['git', 'checkout', branch_name]
-        result.append(subprocess.run(command, cwd=dir_path,  stdout=subprocess.PIPE, stderr=subprocess.PIPE))
-    elif check_if_exists.status_code == 404:
-        command = ['git', 'checkout', '-b', branch_name]
-        result.append(subprocess.run(command, cwd=dir_path,  stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+def push(PAT, user_name, dir_path, repo_name, branch='main', file_path='.', commit_msg='user forgot to add message'):
+    """
+    Function which commits code to local branches
+    PAT - The user's PAT token
+    dir_path - local directory where the code is stored
+    repo_name - name of the repo to which the code is pushed to. 
+    branch - if user wants to switch branch, the name of the new branch, 
+                default is set to main.
+    dir_path - path to where the repo is hosted locally
+    commit_msg - The commit message
+    files - files the user wants to commit
 
-    commands = [
-        ['git', 'add', file_path],
-        ['git', 'commit', '-m', commit_message],
-        ['git', 'push',  f'https://{PAT}@github.com/{user_name}/test2.git']
-    ]
-   
-    for command in commands:
-        result.append(subprocess.run(command, cwd=dir_path,  stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+    make sure the folder name in local matches the repo_name
+    """
     
-
-    return True
+    commit_response = commit(dir_path, branch, file_path, commit_msg)
+    
+    command = ['git', 'push', f'https://{PAT}@github.com/{user_name}/{repo_name}.git']
+    push_response = subprocess.run(command, cwd=dir_path,  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return commit_response, push_response
