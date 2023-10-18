20a5d327f579575f13cce28e5f34fef57773472d
Ayush Agarwal <agarwalayush1996@gmail.com>
2023-10-18 13:08:07 -0400

    Update gits_commit.py copyright

--- src/gits_commit.py
+++ src/gits_commit.py
@@ -1,3 +1,12 @@
+"""
+Copyright (C) 2023 G.I.T.S.3 - All Rights Reserved
+You may use, distribute, and modify this code under the
+terms of the MIT license.
+You should have received a copy of the MIT license with
+this file. If not, please write to: secheaper@gmail.com
+
+"""
+
 import subprocess
 
 def commit(dir_path, change_branch = False, new_branch = 'main', files = '.', commit_msg = 'commiting through UI'):
