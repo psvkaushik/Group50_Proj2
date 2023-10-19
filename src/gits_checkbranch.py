19f8774441f976311f89ad213780513bdec9a911
psvkaushik <psvkaushik@gmail.com>
2023-10-19 19:35:01 -0400

    gcb names

--- src/gits_checkbranch.py
+++ src/gits_checkbranch.py
@@ -9,16 +9,16 @@ this file. If not, please write to: gits3.1project@gmail.com
 
 import requests
 
-def check_branch_exists(PAT, user_name, repo_name, branch_name):
+def check_branch_exists(github_token, user_name, repo_name, branch_name):
     """
     Function to check if a branch exists or not
-    PAT : user's PAT
+    github_token : user's PAT
     branch_name : the name of the branch whose existence is in question
     user_name : url of the repository on which the branch existence is checked 
     """
     headers = {
         "Accept": 'application/vnd.github.v3+json',
-        "Authorization" : f'token {PAT}'
+        "Authorization" : f'token {github_token}'
     }
 
     url = f'https://api.github.com/repos/{user_name}/{repo_name}/branches/{branch_name}'
