381140d2dd9f227f9ab375e19801c1e19b69246c
psvkaushik <psvkaushik@gmail.com>
2023-10-19 19:38:43 -0400

    gdelete names

--- src/gits_delete.py
+++ src/gits_delete.py
@@ -8,22 +8,22 @@ this file. If not, please write to: gits3.1project@gmail.com
 
 import requests
 
-def delete_github_repo(PAT, username, repo_name_to_delete):
+def delete_github_repo(github_token, user_name, repo_name_to_delete):
     """
 
     Function to delete a  github repo
-    PAT : user's PAT token
-    username : the 
+    github_token : user's PAT token
+    user_name : the github username
     repo_name_to_delete : name of the repo to be created
     
 
     """
     headers = {
-        'Authorization': f'token {PAT}',
+        'Authorization': f'token {github_token}',
         'Accept': 'application/vnd.github.v3+json'
     }
 
-    url = f'https://api.github.com/repos/{username}/{repo_name_to_delete}'
+    url = f'https://api.github.com/repos/{user_name}/{repo_name_to_delete}'
 
     response = requests.delete(url, headers=headers)
     
