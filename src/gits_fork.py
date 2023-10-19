142bd345580c7c18f563794adcd8789849435f2d
psvkaushik <psvkaushik@gmail.com>
2023-10-19 19:39:37 -0400

    fork names

--- src/gits_fork.py
+++ src/gits_fork.py
@@ -8,17 +8,17 @@ this file. If not, please write to: gits3.1project@gmail.com
 
 import requests
 
-def fork_repo(target_user, target_repo, PAT):
+def fork_repo(target_user, target_repo, github_token):
     """"
     Forks a repo from the target repo to the user's repo
     target_user : username of the user who's repo you want to fork
     target_repo : name of the Repo which is to be forked
-    PAT : personal PAT
+    github_token : personal PAT
 
     """
     headers = {
         "Accept": 'application/vnd.github.v3+json',
-        "Authorization" : f'token {PAT}'
+        "Authorization" : f'token {github_token}'
     }
 
     url = f'https://api.github.com/repos/{target_user}/{target_repo}/forks'
