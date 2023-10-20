0f74268cfb18a83aae01c90044a724380e575f10
VaishnaviNaik96 <111774368+VaishnaviNaik96@users.noreply.github.com>
2023-10-19 20:38:04 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#Ask for input from user
-
-#read -p "Enter username please: " Username
-#read -p "Enter your auth token please: " Token
-
-
 # Check if pip is installed
 
 if command -v pip &>/dev/null; then
@@ -23,10 +17,6 @@ else
     PIP_COMMAND=pip  # Use pip
 
 fi
-
-# Set GitHub environment variable
-
-export GITHUB_TOKEN=$Token
 
 # Install the required Python packages using the determined pip command
 
